b87632198ef84c4e8292e4d47c4370c96240b6b1
alexfit4 <alex.ashtiany@gmail.com>
2021-03-18 18:32:04 -0400

    fixed nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,12 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from "react-router-dom";
-import {
-	Navbar,
-	Nav,
-} from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import Home from "./Home";
 import About from "./About";
 import SinglePost from "./SinglePost";
 import Post from "./Post";
 import Project from "./Project";
 import Resume from "../components/Amir_Ashtiany_Resume[915].pdf";
-
 
 class Navbars extends React.Component {
 	render() {
@@ -31,16 +23,27 @@ class Navbars extends React.Component {
 										<Nav.Link href="/post">Blog Post</Nav.Link>
 										<Nav.Link href="/project">Projects</Nav.Link>
 										<Nav.Link href="/about">About Me</Nav.Link>
-										<Nav.Link target="_blank" href={Resume}>Resume</Nav.Link>
+										<Nav.Link target="_blank" href={Resume}>
+											Resume
+										</Nav.Link>
 									</Nav>
 									<Nav className="ml-auto">
-										<Nav.Link href="/https://github.com/Alexfit4">
+										<Nav.Link
+											target="_blank"
+											href="https://github.com/Alexfit4"
+										>
 											<i className="fab fa-github text-3xl "></i>
 										</Nav.Link>
-										<Nav.Link href="/https://www.linkedin.com/in/amir-ashtiany-4a25421b9/">
+										<Nav.Link
+											target="_blank"
+											href="https://www.linkedin.com/in/amir-ashtiany-4a25421b9/"
+										>
 											<i className="fab fa-linkedin text-3xl "></i>
 										</Nav.Link>
-										<Nav.Link href="/https://www.facebook.com/alex.ashtiany">
+										<Nav.Link
+											target="_blank"
+											href="https://www.facebook.com/alex.ashtiany"
+										>
 											<i className="fab fa-facebook-square text-3xl "></i>
 										</Nav.Link>
 									</Nav>
